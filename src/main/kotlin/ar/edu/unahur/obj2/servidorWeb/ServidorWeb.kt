4962a52e80d84e32b62f0302daf9d64b8730a6d4
patriciorearte <patriciorearte97@gmail.com>
2021-06-19 20:21:08 -0300

    Analizadores por la mitad

--- src/main/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWeb.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWeb.kt
@@ -12,17 +12,26 @@ enum class CodigoHttp(val codigo: Int) {
 
 class Pedido(val ip: String, val url: URL, val fechaHora: LocalDateTime)
 
-data class Respuesta(val codigo: CodigoHttp, val body: String, val tiempo: Int, val pedido: Pedido)
+data class Respuesta(val codigo: CodigoHttp, val body: String, val tiempo: Int, val pedido: Pedido) {
+
+    fun esRespuestaDemorada(tiempoAnalizadores: Int) = tiempo > tiempoAnalizadores
+}
 
 
 class URL(val protocolo: String, val ruta: String, val extension: String)
 {
   fun cumpleConProtocolo() = protocolo=="http"
+
+    fun esCompatibleConModulo(modulo:Modulo): Boolean {
+        return modulo.estaLaExtension(extension)
+    }
 }
 
 class servidorWeb {
 
-  var modulos = mutableListOf<Modulo>()
+    var analizadores= mutableListOf<Analizadores>()
+
+    var modulos = mutableListOf<Modulo>()
 
   fun agregarModulo(modulo: Modulo) {
     modulos.add(modulo)
@@ -32,24 +41,37 @@ class servidorWeb {
     modulos.remove(modulo)
   }
 
-  fun cumpleConElProtocolo(pedido: Pedido): String {
-    return if(!pedido.url.cumpleConProtocolo()) {
-      Respuesta(CodigoHttp.NOT_IMPLEMENTED, "", 10, pedido).darRespuesta()
+    fun agregarAnalizador(analizador: Analizadores) {
+       analizadores.add(analizador)
+    }
+
+    fun sacarAnalizador(analizador: Analizadores) {
+        analizadores.remove(analizador)
+    }
+
+  fun cumpleConElProtocolo(pedido: Pedido): Respuesta {
+    return if(!pedido.url.cumpleConProtocolo())
+    {
+      Respuesta(CodigoHttp.NOT_IMPLEMENTED, "", 10, pedido)
     }
     else {
-      Respuesta(CodigoHttp.OK, "", 10, pedido).darRespuesta()
+      Respuesta(CodigoHttp.OK, "", 10, pedido)
     }
   }
 
   fun puedeResponder(pedido: Pedido) = modulos.any{m->m.puedeAtenderAlPedido(pedido)}
 
-  fun recibirUnPedido(pedido: Pedido): String {
-    return if(puedeResponder(pedido) and pedido.url.cumpleConProtocolo())  {
-      Respuesta(CodigoHttp.OK, "", 10, pedido).darRespuesta()
+  fun recibirUnPedido(pedido: Pedido): Respuesta {
+    if(puedeResponder(pedido) and pedido.url.cumpleConProtocolo()){
+      val respuesta = Respuesta(CodigoHttp.OK, "Funciona",5, pedido)
+      analizadores.forEach { it.agregarRespuesta(respuesta) }
+
+      return respuesta
     }
     else
     {
-      Respuesta(CodigoHttp.NOT_FOUND, "", 10, pedido).darRespuesta()
+      return Respuesta(CodigoHttp.NOT_FOUND, "", 10, pedido)
+
     }
   }
 }
@@ -57,11 +79,18 @@ class servidorWeb {
 interface Modulo
 {
   fun puedeAtenderAlPedido(pedido: Pedido): Boolean
+  fun estaLaExtension(extension: String): Boolean
 }
 
 object Imagen: Modulo {
   var extenciones = mutableListOf<Any>("jpg", "png", "gif")
   override fun puedeAtenderAlPedido(pedido: Pedido) = extenciones.any { extencion->extencion==pedido.url.extension }
+
+    fun tieneExtension(extension: Any): Boolean {
+        return extenciones.contains(extension)
+    }
+
+    override fun estaLaExtension(extension: String) = extenciones.any{c->c==extension}
 
 }
 
@@ -69,6 +98,39 @@ object Texto: Modulo {
   var extenciones = mutableListOf<Any>("docx", "odt")
   override fun puedeAtenderAlPedido(pedido: Pedido)= extenciones.any { extencion->extencion==pedido.url.extension }
 
+    override fun estaLaExtension(extension: String) = Imagen.extenciones.any{ c->c==extension}
 }
 
 class Analizadores(val demoraMinima:Int,var ipsPeligrosas:List<Any>)
+{
+    var respuestas = mutableListOf<Respuesta>()
+    var respuestasDeIpsSospechosas=mutableListOf<Respuesta>()
+
+    fun agregarRespuesta(respuesta: Respuesta) {
+        respuestas.add(respuesta)
+    }
+
+    fun cantidadDeRespuestasDemoradasDelModulo(modulo: Modulo): Int {
+        return respuestas.filter { c-> c.pedido.url.esCompatibleConModulo(modulo)}.count{c->c.esRespuestaDemorada(demoraMinima)}
+
+    }
+
+
+
+    fun respuestasConIpsSospechosas(): Int {
+        val ips = respuestas.map{it.pedido.ip}
+        return ips.intersect(ipsPeligrosas).size
+    }
+
+    fun elModuloMasConsultadoPorIps():Modulo
+    {
+        val pedidosImagen = respuestas.map{it.pedido.url.esCompatibleConModulo(Imagen)}.size
+        val pedidosTexto = respuestas.map{it.pedido.url.esCompatibleConModulo(Texto)}.size
+        if(pedidosImagen>pedidosTexto)
+        {return Imagen}
+        else{return Texto}
+    }
+
+
+
+}
--- src/test/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWebTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWebTest.kt
@@ -10,23 +10,32 @@ class ServidorWebTest : DescribeSpec({
   describe("Un servidor web") {
     val servidor1=servidorWeb()
 
-    val url1=URL("http","documents/index","html")
+    val url1=URL("http","documents/index","jpg")
     val url2=URL("https","desktops/download","jpg")
 
     val pedido1=Pedido("1.10.11:200",url1,LocalDateTime.of(2021,6,20, 1,30, 0))
     val pedido2=Pedido("2.1.10:2010",url2,LocalDateTime.of(2021,4,20,5,2,10))
 
     it("Requerimiento 1 sin modulo") {
-      servidor1.cumpleConElProtocolo(pedido1).shouldBe("OK. Tiempo de respuesta: 10")
+      servidor1.cumpleConElProtocolo(pedido1).shouldBe(Respuesta(CodigoHttp.OK, "", 10, pedido1))
 
-      servidor1.cumpleConElProtocolo(pedido2).shouldBe("NOT_IMPLEMENTED. Tiempo de respuesta: 10")
+      servidor1.cumpleConElProtocolo(pedido2).shouldBe(Respuesta(CodigoHttp.NOT_IMPLEMENTED, "", 10, pedido2))
     }
     it("Requerimiento 2 con modulo") {
       val servidor2 = servidorWeb()
-      servidor2.recibirUnPedido(pedido2).shouldBe("NOT_FOUND. Tiempo de respuesta: 10")
+      servidor2.recibirUnPedido(pedido1).shouldBe(Respuesta(CodigoHttp.NOT_FOUND, "", 10, pedido1))
       servidor2.agregarModulo(Imagen)
-      servidor2.recibirUnPedido(pedido2).shouldBe("OK. Tiempo de respuesta: 10. Generado por el pedido: pedido2")
+      servidor2.recibirUnPedido(pedido1).shouldBe(Respuesta(CodigoHttp.OK,"Funciona",5,pedido1))
     }
+      it("Requerimiento 3 con analizadores")
+      {
+        val analizador1=Analizadores(4, listOf("12312", "123124334") )
+        val servidor3 = servidorWeb()
+        servidor3.agregarModulo(Imagen)
+          servidor3.agregarAnalizador(analizador1)
+          servidor3.recibirUnPedido(pedido1).shouldBe(Respuesta(CodigoHttp.OK, "Funciona", 5, pedido1))
+          analizador1.cantidadDeRespuestasDemoradasDelModulo(Imagen).shouldBe(1)
+      }
   }
 })
 
