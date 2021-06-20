aa3cf664a83ed11f5bcfdf1b4535b2c901ea945f
patriciorearte <patriciorearte97@gmail.com>
2021-06-19 22:47:04 -0300

    Test de estadisticas falta el de local date

--- src/test/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWebTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWebTest.kt
@@ -2,6 +2,7 @@ package ar.edu.unahur.obj2.servidorWeb
 
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
+import java.awt.Image
 import java.time.LocalDate
 import java.time.LocalDateTime
 
@@ -9,13 +10,20 @@ class ServidorWebTest : DescribeSpec({
 
   describe("Un servidor web") {
     val servidor1=servidorWeb()
+      val servidor4=servidorWeb()
 
     val url1=URL("http","documents/index","jpg")
     val url2=URL("https","desktops/download","jpg")
+    val url5=URL("http","desktops/objetos2","html")
+      val url6=URL("htttp","desktop/unaHur","html")
 
-    val pedido1=Pedido("1.10.11:200",url1,LocalDateTime.of(2021,6,20, 1,30, 0))
-    val pedido2=Pedido("2.1.10:2010",url2,LocalDateTime.of(2021,4,20,5,2,10))
+    val pedido1=Pedido("1.10.11:200",url1,LocalDateTime.of(2021,6,20, 1,30))
+    val pedido2=Pedido("2.1.10:2010",url2,LocalDateTime.of(2021,4,20,5,2))
+      val pedido5=Pedido("20.3.24:2650",url5,LocalDateTime.of(2021,3,4,20,5))
+      val pedido6=Pedido("30.36.45:4020",url6,LocalDateTime.of(2021,6,8,4,6))
 
+      val analizador2= Analizadores(5, listOf("2.3.2:210","5.2.7:25000"))
+      val analizador3=Analizadores(6, listOf("3.6.8:9888","65.54.7:1233"))
     it("Requerimiento 1 sin modulo") {
       servidor1.cumpleConElProtocolo(pedido1).shouldBe(Respuesta(CodigoHttp.OK, "", 10, pedido1))
 
@@ -27,7 +35,7 @@ class ServidorWebTest : DescribeSpec({
       servidor2.agregarModulo(Imagen)
       servidor2.recibirUnPedido(pedido1).shouldBe(Respuesta(CodigoHttp.OK,"Funciona",5,pedido1))
     }
-      it("Requerimiento 3 con analizadores ips Sopechosas")
+      it("Requerimiento 3 con analizadores ips Sopechosas ")
       {
         val analizador1=Analizadores(4, listOf("12312", "123124334") )
         val servidor3 = servidorWeb()
@@ -36,9 +44,41 @@ class ServidorWebTest : DescribeSpec({
           servidor3.recibirUnPedido(pedido1).shouldBe(Respuesta(CodigoHttp.OK, "Funciona", 5, pedido1))
           analizador1.cantidadDeRespuestasDemoradasDelModulo(Imagen).shouldBe(1)
       }
-      it("Requerimiento 3 con analizadores estadisticas")
+      it("Analizadores-> tiempo de respuesta promedio")
+      {
+          servidor1. agregarModulo(Imagen)
+          servidor1.agregarAnalizador(analizador2)
+          servidor1.recibirUnPedido(pedido5)
+          servidor1.recibirUnPedido(pedido1)
+          servidor1.recibirUnPedido(pedido2)
+          servidor1.recibirUnPedido(pedido6)
+          analizador2.tiempoDeRepuestaPromedio().shouldBe(5)
+
+      }
+      it("Analizadores ->cantidad de pedidos entre dos momentos")
       {
 
+      }
+      it("Analizadores->cantidad de respuestas cuyo body incluye un determinado String")
+      {
+          servidor1. agregarModulo(Imagen)
+          servidor1.agregarAnalizador(analizador2)
+          servidor1.recibirUnPedido(pedido5)
+          servidor1.recibirUnPedido(pedido1)
+          servidor1.recibirUnPedido(pedido2)
+          servidor1.recibirUnPedido(pedido6)
+          analizador2.cantidadDeRespuetaCutoBodyIncluye(Regex("hola")).shouldBe(0)
+
+      }
+      it("Analizadores -> porcentaje de pedidos con respuesta exitosa")
+      {
+          servidor1. agregarModulo(Imagen)
+          servidor1.agregarAnalizador(analizador2)
+          servidor1.recibirUnPedido(pedido5)
+          servidor1.recibirUnPedido(pedido1)
+          servidor1.recibirUnPedido(pedido2)
+          servidor1.recibirUnPedido(pedido6)
+            analizador2.porcentajeDeRespuestasExitosas().shouldBe(1)
       }
   }
 })
